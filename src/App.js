6cb18b194384ff550679018c463b19abd88ce77e
arif-hstu <arif.dvm.hstu@gmail.com>
2021-03-11 23:38:37 +0600

    revision

--- src/App.js
+++ src/App.js
@@ -12,17 +12,10 @@ import {
   Switch
 } from 'react-router-dom';
 import Details from './Components/Details/Details';
-import { createContext } from 'react';
 
-
-// fetch data from the api
-
-// creating context for passing fetched data
-// export const FetchedContext = createContext();
 
 function App() {
   return (
-    // <FetchedContext.Provider value='arif' >
     <div className='app'>
       <Router>
         <Switch>
@@ -37,7 +30,6 @@ function App() {
           </Route>
         </Switch>
       </Router>
-    {/* // </FetchedContext.Provider> */}
     </div>
   );
 }
